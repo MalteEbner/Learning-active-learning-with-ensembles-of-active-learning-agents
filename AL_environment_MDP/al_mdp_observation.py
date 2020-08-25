bugfix in computation of batch-dependant features

AL_environment_MDP/al_mdp_observation.py
@@ -13,39 +13,41 @@ class Observation:
         self.unlabelled_IDs = unlabelled_IDs
         self.batch_IDs = batch_IDs
 
-        self.features = dict()
+        self.batch_independent_features = dict()
 
-    def delete_features_of_chosen_samples(self,sample_IDs: List[int]):
-        for (feature_name,feature_value) in self.features.items():
-            feature_value_with_deleted_sample = np.delete(feature_value,sample_IDs,axis=0)
-            self.features[feature_name] = feature_value_with_deleted_sample
-        self.unlabelled_IDs = np.delete(self.unlabelled_IDs,sample_IDs,axis=0)
+    def update_features_based_on_action(self, action_sample_IDs: List[int]):
+        if hasattr(self, "min_distances_to_labelled_batch"):
+            delattr(self, "min_distances_to_labelled_batch")
+        for (feature_name,feature_value) in self.batch_independent_features.items():
+            feature_value_with_deleted_sample = np.delete(feature_value, action_sample_IDs, axis=0)
+            self.batch_independent_features[feature_name] = feature_value_with_deleted_sample
+        self.unlabelled_IDs = np.delete(self.unlabelled_IDs, action_sample_IDs, axis=0)
 
     def get_prediction_entropies(self):
-        if not 'predictions' in self.features:
-            self.features['predictions'] = self.task.getPredictions(self.unlabelled_IDs)
-        if not 'prediction_entropies' in self.features:
-            probs = self.features['predictions']
+        if not 'predictions' in self.batch_independent_features:
+            self.batch_independent_features['predictions'] = self.task.getPredictions(self.unlabelled_IDs)
+        if not 'prediction_entropies' in self.batch_independent_features:
+            probs = self.batch_independent_features['predictions']
             zeros = np.zeros_like(probs)
             log_probs = np.log2(probs, out=zeros, where=probs > 0)
             entropies = -1 * np.sum(probs * log_probs, axis=1)
-            self.features['prediction_entropies'] = entropies
-        return self.features['prediction_entropies']
+            self.batch_independent_features['prediction_entropies'] = entropies
+        return self.batch_independent_features['prediction_entropies']
 
     def get_min_distances_to_labelled_and_batch(self):
-        if not 'min_distances_to_labelled_batch' in self.features:
+        if not hasattr(self,"min_distances_to_labelled_batch"):
             labelled_batch_IDs = self.labelled_IDs + self.batch_IDs
             labelled_batch_samples = self.task.get_samples_repr_1d(labelled_batch_IDs)
             unlabelled_samples = self.task.get_samples_repr_1d(self.unlabelled_IDs)
 
             (_, distances_min) = pairwise_distances_argmin_min(
                 unlabelled_samples, labelled_batch_samples)
-            self.features['min_distances_to_labelled_batch'] = distances_min
+            self.min_distances_to_labelled_batch = distances_min
 
-        return self.features['min_distances_to_labelled_batch']
+        return self.min_distances_to_labelled_batch
 
     def get_distances_percentiles_to_all(self, percentile_borders: List[float] = [0.05]):
-        if not 'distances_percentiles_to_all' in self.features:
+        if not 'distances_percentiles_to_all' in self.batch_independent_features:
             unlabelled_samples = self.task.get_samples_repr_1d(self.unlabelled_IDs)
 
             no_unlabelled_samples_for_representativeness = min(500, len(self.unlabelled_IDs))
@@ -55,9 +57,9 @@ class Observation:
 
             distances_to_Samples = self._get_similarity_percentiles(
                 unlabelled_samples, all_samples_subset, percentile_borders)
-            self.features['distances_percentiles_to_all'] = distances_to_Samples
+            self.batch_independent_features['distances_percentiles_to_all'] = distances_to_Samples
 
-        return self.features['distances_percentiles_to_all']
+        return self.batch_independent_features['distances_percentiles_to_all']
 
     def _get_similarity_percentiles(self, vectorA: np.ndarray, vectorB: np.ndarray,
                                     percentile_borders: List[float]) -> np.ndarray: