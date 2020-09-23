3b7cf9785245bd3d9ae0fc9a568314594b225a8f
MalteEbner <malte.ebner@gmail.com>
2020-09-23 17:13:06 +0200

    reworked tests to be applied on all tasks

--- pytests/test_1_AL_environment_MDP.py
+++ pytests/test_1_AL_environment_MDP.py
@@ -1,22 +1,37 @@
+import pytest
+
 from AL_environment_MDP.al_parameters import AL_Parameters
 from supervised_learning_tasks.task_parameters import Task_Parameters
 from AL_environment_MDP.al_env import AL_Env
 
-def test_environment():
+
+def _test_environment(task_name):
     al_parameters = AL_Parameters(startingSize=8,annotationBudget=12)
     al_parameters.batchSize_annotation = 4
+
+    task_params = Task_Parameters(taskName=task_name)
+    task = task_params.createTask()
+
+    al_env = AL_Env(al_parameters, task)
+    al_env.reset()
+
+    iteration = 0
+    expectedNoIterations = al_env.expectedNoIterations()
+    for i in range(expectedNoIterations):
+        action = [0]
+        newObservation, reward, done, info = al_env.step(action)
+        iteration += 1
+        if done:
+            break
+
+def _get_test_parameters():
+    test_cases = []
     for task_name in ["model_UCI", "model_checkerboard", "model_Vision", "model_bAbI"]:
-        task_params = Task_Parameters(taskName=task_name)
-        task = task_params.createTask()
+        name = f'{task_name}'
+        test_case = pytest.param(task_name, id=name)
+        test_cases.append(test_case)
+    return test_cases
 
-        al_env = AL_Env(al_parameters, task)
-        al_env.reset()
-
-        iteration = 0
-        expectedNoIterations = al_env.expectedNoIterations()
-        for i in range(expectedNoIterations):
-            action = [0]
-            newObservation, reward, done, info = al_env.step(action)
-            iteration += 1
-            if done:
-                break
+@pytest.mark.parametrize("task_name", _get_test_parameters())
+def test_heuristics(task_name):
+    _test_environment(task_name)
--- pytests/test_3_application_handler.py
+++ pytests/test_3_application_handler.py
@@ -1,16 +1,17 @@
+import pytest
+
 from AL_apply_agent_on_task.application_config import get_application_config
 from AL_agents.al_agent_parameters import AL_Agent_Parameters
 from AL_apply_agent_on_task.application_handler_file_handler import ApplicationHandlerFileHandlerJSON
 from AL_apply_agent_on_task.application_handler import ApplicationHandler
 
 
-def test_application_handler():
-    taskName = ["model_UCI", "model_checkerboard", "model_Vision", "model_bAbI_memoryNetwork"][0]
+def _test_application_handler(task_name):
     test = True
 
     # define task
     (task_params, base_dataset, usualBatchSize, usualBatchSize_random,
-     al_params, n_jobs, noRepetitions) = get_application_config(taskName)
+     al_params, n_jobs, noRepetitions) = get_application_config(task_name)
 
     # define application handler
     agent_params = AL_Agent_Parameters(agentName="Random",batchSize_annotation=4,batchSize_agent=-1)
@@ -28,3 +29,15 @@ def test_application_handler():
 
     # plot the results
     ApplicationHandlerFileHandlerJSON(filename).plotAllContentWithConfidenceIntervals()
+
+def _get_test_parameters():
+    test_cases = []
+    for task_name in ["model_UCI", "model_checkerboard", "model_Vision", "model_bAbI"]:
+        name = f'{task_name}'
+        test_case = pytest.param(task_name, id=name)
+        test_cases.append(test_case)
+    return test_cases
+
+@pytest.mark.parametrize("task_name", _get_test_parameters())
+def test_heuristics(task_name):
+    _test_application_handler(task_name)
