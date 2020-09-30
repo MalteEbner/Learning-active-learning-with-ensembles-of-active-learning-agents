04e3334046fdd4d7fe1433921d19cbef7468bc05
MalteEbner <malte.ebner@gmail.com>
2020-09-30 18:48:20 +0200

    parallel_run_handler now uses verbose argument

--- AL_apply_agent_on_task/parallel_run_handler.py
+++ AL_apply_agent_on_task/parallel_run_handler.py
@@ -105,8 +105,9 @@ class ParallelRunHandler:
             for i, applicationHandler in enumerate(application_handlers):
                 applicationHandler.run_episode()
                 finished_application_handlers += [applicationHandler]
-                print(f'finished {i+1} '
-                      f'of {len(agent_params_list)} tasks at time {time.time() - start}')
+                if self.verbose:
+                    print(f'finished {i+1} '
+                          f'of {len(agent_params_list)} tasks at time {time.time() - start}')
                 if i % 10 == 9 and self.save_results:
                     self.fileHandler.write_application_handlers_to_file(finished_application_handlers)
                     finished_application_handlers = []
