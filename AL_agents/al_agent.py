2c8c8b3393817203d542d0a450b42100bab15ddb
MalteEbner <malte.ebner@gmail.com>
2020-11-17 20:10:59 +0100

    bugfix in agent to support annotation budget > no samples

--- AL_agents/al_agent.py
+++ AL_agents/al_agent.py
@@ -13,6 +13,7 @@ class ALAgent:
             raise ValueError
 
         batch_size = self.al_agent_parameters.batch_size_agent
+        batch_size = min(batch_size, len(utilities))
         samples_with_maximum_utility = np.argpartition(utilities, -batch_size)[-batch_size:]
 
         action = samples_with_maximum_utility
