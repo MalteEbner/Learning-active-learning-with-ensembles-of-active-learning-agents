2c913e44bcbf3a8755aeef86e9873f1b15755817
MalteEbner <malte.ebner@gmail.com>
2020-11-18 13:01:11 +0100

    added experiments with ensemble with equal weights

--- Experiments/results/concat_two_appliction_handlers.py
+++ Experiments/results/concat_two_appliction_handlers.py
@@ -1,7 +1,7 @@
 from AL_apply_agent_on_task.application_handler_file_handler import ApplicationHandlerFileHandlerJSON
 
-handler_1 = "Experiments/results/bAbI two supporting facts experiments.json"
-handler_2 = "Experiments/results/model_bAbI two_supporting_facts_experiments.json"
+handler_1 = "Experiments/results/fashion experiments.json"
+handler_2 = "Experiments/results/fashion-MNIST experiments.json"
 
 
 handler_1 = ApplicationHandlerFileHandlerJSON(handler_1)
